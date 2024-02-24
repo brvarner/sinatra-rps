077cd8b74086db3d33f3d896175ad40526a3f216
Brandon Varner <84243251+brvarner@users.noreply.github.com>
2024-02-24 22:42:06 +0000

    added routes, now adding navbar and logic

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 require 'sinatra'
 
+opt_array = ["paper", "rock", "scissors"]
+
 get('/') do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/paper") do
+  erb(:paper)
+end
+
+get("/rock") do
+  erb(:rock)
+end
+
+get("/scissors") do
+  erb(:scissors)
 end
